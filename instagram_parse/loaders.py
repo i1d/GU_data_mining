aeb15f25a069c424c3398243214349a9d8b7e1d4
i1d <ivan.lamden@gmail.com>
2021-04-20 19:58:25 +0300

    lesson_7 commit

--- instagram_parse/loaders.py
+++ instagram_parse/loaders.py
@@ -1,37 +1,8 @@
-import re
 from scrapy.loader import ItemLoader
-from scrapy import Selector
 from itemloaders.processors import TakeFirst, MapCompose, Join
-
-
-# def clear_price(price: str) -> float:
-#     try:
-#         result = float(price)
-#     except ValueError:
-#         result = None
-#     return result
-#
-# def get_author_link(link):
-#     author_link = f"https://hh.ru{link}"
-#     return author_link
 
 
 class InstagramLoader(ItemLoader):
     default_item_class = dict
     date_parse_out = TakeFirst()
     data_out = TakeFirst()
-    # url_out = TakeFirst()
-    # # title_out = TakeFirst()
-    # price_in = MapCompose(clear_price)
-    # price_out = TakeFirst()
-    # address_out = TakeFirst()
-    # # description_out = Join()
-    # author_link_out = TakeFirst()
-    # # # author_link_in = MapCompose(get_author_link)
-
-
-# class HhruAuthorLoader(ItemLoader):
-#     default_item_class = dict
-#     url_out = TakeFirst()
-#     author_out = Join()
-#     website_out = TakeFirst()
--- instagram_parse/spiders/instagram.py
+++ instagram_parse/spiders/instagram.py
@@ -6,10 +6,11 @@ from datetime import datetime
 
 class InstagramSpider(scrapy.Spider):
     name = 'instagram'
-    allowed_domains = ['www.instagram.com']
+    allowed_domains = ['www.instagram.com', 'i.instagram.com']
     start_urls = ['https://www.instagram.com/']
     _login_url = 'https://www.instagram.com/accounts/login/ajax/'
     _tag_path = '/explore/tags/'
+    _api_url = 'https://i.instagram.com/api/v1/tags/'
 
     def __init__(self, username, enc_password, tags, *args, **kwargs):
         super(InstagramSpider, self).__init__(*args, **kwargs)
@@ -25,28 +26,9 @@ class InstagramSpider(scrapy.Spider):
     # data - данные полученые от инстаграм
     # Скачать изображения всех постов и сохранить на диск
 
-    _xpath_data_query = {
-        "title": '//div[@class="title-info-main"]/span/text()',
-        "price": '//div[@class="item-price-wrapper"]//span[@class="js-item-price"]/@content',
-        "address": '//div[@class="item-address"]//span[@class="item-address__string"]/text()',
-        "parameters": '//div[@class="item-params"]/text()',
-        "author_link": '//div[@class="seller-info-value"]//a/@href',
-    }
-
-    _xpaths_selectors = {
-        "pagination": "//div[@class='pagination-hidden-3jtv4']//a/@href",
-        "flat": "//div[@class='iva-item-titleStep-2bjuh']//a/@href",
-    }
-
-
-
     def js_data_extract(self, response):
         script = response.xpath('//body/script[contains(text(), "window._sharedData")]/text()').extract_first()
         return json.loads(script[script.index("{"):-1])
-
-    def _get_follow_xpath(self, response, selector, callback):
-        for link in response.xpath(selector):
-            yield response.follow(link, callback=callback)
 
     def auth(self, response):
         js_data = self.js_data_extract(response)
@@ -65,20 +47,24 @@ class InstagramSpider(scrapy.Spider):
         if b"json" in response.headers['Content-Type']:
             if response.json().get('authenticated'):
                 for tag in self.tags:
+               #     yield response.follow(f'{self._tag_path}{tag}/', callback=self.tag_parse)  #todo: починить
                     yield response.follow(f'{self._tag_path}{tag}/', callback=self.post_parse)
+                 #   yield response.follow(f'{self._api_url}{tag}/sections/', callback=self.post_parse)
         else:
             yield self.auth(response)
 
     def tag_parse(self, response):
         js_data = self.js_data_extract(response)
-        print(1)
-        # section = js_data['entry_data']['TagPage'][0]['data']['top']['sections']
+        tag_data = js_data['entry_data']['TagPage'][0]['data']
+        loader = InstagramLoader(response=response)
+        loader.add_value("date_parse", datetime.now().strftime('%d.%m.%Y_%H:%M:%S'))
+        loader.add_value("data", tag_data)
+        yield loader.load_item()
+     #   yield self.post_parse
+     #   yield response.follow(f'{self._api_url}{self.tag}/sections/', callback=self.post_parse)
 
-
-        # js_object = js_data['sections'][]
     def post_parse(self, response):
         js_data = self.js_data_extract(response)
-
         sections = js_data['entry_data']['TagPage'][0]['data']['top']['sections']  # list
         for section in sections:
             medias = section['layout_content']['medias']  # list
@@ -88,24 +74,3 @@ class InstagramSpider(scrapy.Spider):
                 loader.add_value("date_parse", datetime.now().strftime('%d.%m.%Y_%H:%M:%S'))
                 loader.add_value("data", data)
                 yield loader.load_item()
-
-
-    # def flat_parse(self, response):
-    #     loader = InstagramLoader(response=response)
-    #     loader.add_value("url", response.url)
-    #     for key, xpath in self._xpath_data_query.items():
-    #         loader.add_xpath(key, xpath)
-    #     yield loader.load_item()
-        # yield from self._get_follow_xpath(
-        #     response, self._xpaths_selectors["author"], self.author_parse
-        # )
-
-    # def author_parse(self, response):
-    #     author_loader = HhruAuthorLoader(response=response)
-    #     author_loader.add_value("url", response.url)
-    #     for key, xpath in self._xpath_author_query.items():
-    #         author_loader.add_xpath(key, xpath)
-    #     yield author_loader.load_item()
-    #     yield from self._get_follow_xpath(
-    #         response, self._xpaths_selectors["author_vacancies"], self.parse
-    #     )
